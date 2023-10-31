74a4444719599ed7aed49fd4d0da971ee5bfbe85
Akshay <akshayjec26@gmail.com>
2023-10-31 12:33:00 +0530

    java program 1st

---
+++ src/test/java/com/actitime/testcript/CreateCustomer.java
@@ -0,0 +1,20 @@
+package com.actitime.testcript;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class CreateCustomer {
+	static {
+		System.setProperty("webdriver.chrome.driver","./src/main/resources/driver/chromedriver.exe");
+	}
+	@Test
+	public void login() {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://demo.actitime.com/");
+		driver.findElement(By.id("username")).sendKeys("admin");
+		driver.findElement(By.name("pwd")).sendKeys("manager");
+		driver.findElement(By.xpath("//div[text()='Login ']")).click();
+	}
+}
